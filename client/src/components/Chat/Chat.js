d5016e1b9a60f4dca0f9ab5a14bfa7f0bb45a465
jjgesulgon <jj@hybrain.co>
2020-06-20 01:28:51 +0800

    build

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:5000'
+    const ENDPOINT = 'http://134.209.104.173:5000/'
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
