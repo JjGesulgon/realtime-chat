c186d7ba71d1ce86c220d33f0f7d6f3b32e8daef
jjgesulgon <jj@hybrain.co>
2020-06-20 11:01:58 +0800

    build

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'https://server.sturya.jjgesulgon.dev/'
+    const ENDPOINT = 'https://api.sturya.jjgesulgon.dev/'
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
